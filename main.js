f3498e94b7d3ac72cd32575a03100908df2d7d0b
mtsgi <oq@live.jp>
2020-06-12 22:38:41 +0900

    おとふだびよりをAxiosで通信

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ import UIkit from "uikit";
 import Icons from "uikit/dist/js/uikit-icons";
 import "uikit/dist/css/uikit.css";
 import "uikit/dist/css/uikit.min.css";
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
 UIkit.use(Icons);
+Vue.use(VueAxios, axios);
